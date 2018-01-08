56b443894deac5b53e49d21950aab02cd61c92f2
owen.chou <owen.chou@presco.ws>
2018-01-08 14:36:00 +0800

    fix store

--- ShopSystem/Program.cs
+++ ShopSystem/Program.cs
@@ -15,81 +15,13 @@ namespace ShopSystem
             var store = new Store();
             while (number != "0")
             {
-                Console.Write("請輸入數字: 1、二手蘋果手機，2、C# cookbook，3、HP 筆電，  0 結束");
-                number = Console.ReadLine();
-                store.addCartItem(number);
-                Console.Write("目前商品");
-                store.PrintAll();
+                store.PrintIntroduction();
+                store.shoppingCart.AddCartItem(Console.ReadLine());
+                store.PrintAllShoppingItem();
             }
 
             Console.Write("按下ENTER結束");
             Console.ReadLine();
         }
-    }
-
-    internal class ShopItem
-    {
-        public int id;
-
-        public string name;
-
-        public double price;
-    }
-
-    public class Store
-    {
-        private List<ShopItem> ShopItemList = new List<ShopItem>();
-
-        public enum ItemType { 二手蘋果手機 = 1, cookbook = 2, HP筆電 = 3, 其他 = -1 };
-
-        public void addCartItem(string type) {
-            var t = ItemType.其他;
-            switch (type)
-            {
-                case "1":
-                    t = ItemType.二手蘋果手機;
-                    break;
-                case "2":
-                    t = ItemType.cookbook;
-                    break;
-                case "3":
-                    t = ItemType.HP筆電;
-                    break;
-                default:
-                    break;
-            }
-            addCartItem(t);
-        }
-
-        public void addCartItem(ItemType type)
-        {
-            switch (type)
-            {
-                case ItemType.二手蘋果手機:
-                    ShopItemList.Add(new ShopItem() { id = 1, name = "二手蘋果手機", price = 8700 });
-                    break;
-                case ItemType.cookbook:
-                    ShopItemList.Add(new ShopItem() { id = 2, name = "C# cookbook", price = 568 });
-                    break;
-                case ItemType.HP筆電:
-                    ShopItemList.Add(new ShopItem() { id = 3, name = "HP 筆電", price = 16888 });
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        public void PrintAll()
-        {
-            foreach (var item in ShopItemList)
-            {
-                Console.Write("Name :{0}", item.name);
-            }
-            Console.WriteLine();
-
-            Console.Write("目前商品總金額 :{0}", ShopItemList.Sum(i => i.price));
-            Console.WriteLine();
-        }
-
     }
 }
---
+++ ShopSystem/Store.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopSystem
+{
+    public class Store
+    {
+        public ShoppingCart shoppingCart = new ShoppingCart();
+
+        public void PrintIntroduction()
+        {
+            Console.Write("請輸入數字:");
+            foreach (var item in shoppingCart.shopItemList)
+            {
+                Console.Write("{1}、Name :{0}", item.id, item.name);
+            }
+            Console.Write("，  0 結束");
+            Console.WriteLine();
+        }
+        
+        public void PrintAllShoppingItem()
+        {
+            Console.Write("目前商品");
+            foreach (var item in shoppingCart.shopItemList)
+            {
+                Console.Write("Name :{0}", item.name);
+            }
+            Console.WriteLine();
+
+            Console.Write("目前商品總金額 :{0}", shoppingCart.shopItemList.Sum(i => i.price));
+            Console.WriteLine();
+        }
+    }
+
+    public class ShoppingCart
+    {
+        public List<Product> shopItemList = new List<Product>();
+     
+        public void AddCartItem(string type)
+        {
+            shopItemList.Add(Product.getProductByID(type));
+        }
+    }
+
+    public class Product
+    {
+        public int id;
+
+        public string name;
+
+        public double price;
+
+        public Product(int id, string name, int price)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+
+        static public Product getProductByID(string id)
+        {
+            switch (id)
+            {
+                case "1":
+                    return new Product(1, "二手蘋果手機", 8700);
+                case "2":
+                    return new Product(2, "C# cookbook", 568);
+                case "3":
+                    return new Product(3, "HP 筆電", 16888);
+                default:
+                    return null;
+            }
+        }
+    }
+
+}
