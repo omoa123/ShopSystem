b301466c79cd9bae91fa353d9912e7b115028e5c
owen.chou <owen.chou@presco.ws>
2018-01-09 14:04:56 +0800

    fix for issue

--- ShopSystem/Store.cs
+++ ShopSystem/Store.cs
@@ -7,13 +7,14 @@ namespace ShopSystem
 
     public class ShoppingCart
     {
-        public List<Product> shopItemList = new List<Product>();
+        private List<string> shopItemIDList = new List<string>();
 
         public void PrintIntroduction()
         {
             Console.Write("請輸入數字:");
-            foreach (var item in Product.SampleList)
+            foreach (var Sample in ProductManagement.SampleList)
             {
+                var item = Sample.Value;
                 Console.Write(" *{0}* :{1} 、", item.id, item.name);
             }
             Console.Write("，  0 結束");
@@ -22,14 +23,22 @@ namespace ShopSystem
 
         public double GetShopItemSum()
         {
-            return shopItemList != null ? shopItemList.Sum(i => i.price) : 0;
+            var sum = 0.0;
+            foreach (var itemID in shopItemIDList)
+            {
+                var item = ProductManagement.getProductByID(itemID);
+                if (item != null)
+                    sum += item.price;
+            }
+            return sum;
         }
 
         public void PrintAllShoppingItem()
         {
             Console.Write("目前商品  ");
-            foreach (var item in shopItemList)
+            foreach (var itemID in shopItemIDList)
             {
+                var item = ProductManagement.getProductByID(itemID);
                 Console.Write("{0}、", item.name);
             }
             Console.WriteLine();
@@ -40,10 +49,38 @@ namespace ShopSystem
 
         public void AddCartItem(string type)
         {
-            var product = Product.getProductByID(type);
+            var product = ProductManagement.getProductByID(type);
             if (product != null)
-                shopItemList.Add(product);
+                shopItemIDList.Add(type);
         }
+    }
+
+    public class ProductManagement
+    {
+        static public Dictionary<int,Product> SampleList
+        {
+            get
+            {
+                var SampleList = new Dictionary<int, Product>();
+                SampleList.Add(1, new Product(1, "二手蘋果手機", 8700));
+                SampleList.Add(2, new Product(2, "C# cookbook", 568));
+                SampleList.Add(3, new Product(3, "HP 筆電", 16888));
+                return SampleList;
+            }
+        }
+
+        static public Product getProductByID(string id)
+        {
+            try
+            {
+                return SampleList.FirstOrDefault(i => i.Key == int.Parse(id)).Value;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
     }
 
     public class Product
@@ -60,31 +97,6 @@ namespace ShopSystem
             this.name = name;
             this.price = price;
         }
-
-        static public List<Product> SampleList
-        {
-            get
-            {
-                var SampleList = new List<Product>();
-                SampleList.Add(new Product(1, "二手蘋果手機", 8700));
-                SampleList.Add(new Product(2, "C# cookbook", 568));
-                SampleList.Add(new Product(3, "HP 筆電", 16888));
-                return SampleList;
-            }
-        }
-
-        static public Product getProductByID(string id)
-        {
-            try
-            {
-                return SampleList.FirstOrDefault(i => i.id == int.Parse(id));
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
     }
 
 }
